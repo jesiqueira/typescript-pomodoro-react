b7897c98f947bbb279ffe3013550b3d9c06b1ef1
Jose Edmar de Siqueira <edmar.ade@gmail.com>
2025-10-03 07:28:58 -0300

    criado hook useInterval

---
+++ src/hooks/use-interval.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export function useInterval(callback: () => void, delay: number | null) {
+  const savedCallback = React.useRef<() => void | null>(null)
+
+  // Remember the latest callback.
+  React.useEffect(() => {
+    savedCallback.current = callback
+  }, [callback])
+
+  // Set up the interval.
+  React.useEffect(() => {
+    function tick() {
+      savedCallback.current && savedCallback.current()
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay)
+      return () => clearInterval(id)
+    }
+  }, [delay])
+}
