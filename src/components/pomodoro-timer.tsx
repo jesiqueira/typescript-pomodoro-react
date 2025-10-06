84a51d68dce9c30f95e928171bae552d7756887c
Jose Edmar de Siqueira <edmar.ade@gmail.com>
2025-10-06 14:08:56 -0300

    modificado class de components pomodoro-timer

--- src/components/pomodoro-timer.tsx
+++ src/components/pomodoro-timer.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useInterval } from '../hooks/use-interval'
 import Button from './button'
 import Timer from './timer'
@@ -12,19 +12,40 @@ type PomodoroTimerProps = {
 
 const PomodoroTimer = ({ pomodoroTime = 1500 }: PomodoroTimerProps) => {
   const [mainTimer, setMainTimer] = React.useState(pomodoroTime)
+  const [isActive, setIsActive] = React.useState(false)
+  const [isWorking, setisWorking] = React.useState(false)
 
-  useInterval(() => {
-    setMainTimer((prev) => (prev > 0 ? prev - 1 : 0))
-  }, 1000)
+  useEffect(() => {
+    if (isWorking) document.body.classList.add('working')
+    if (!isWorking) document.body.classList.remove('working')
+  }, [isWorking])
+
+  useInterval(
+    () => {
+      setMainTimer((prev) => (prev > 0 ? prev - 1 : 0))
+    },
+    isActive ? 1000 : null,
+  )
+
+  const configureWork = () => {
+    setisWorking(true)
+    setIsActive(true)
+    setMainTimer(pomodoroTime)
+  }
+
+  const configureRest = () => {
+    setisWorking(false)
+    setIsActive(false)
+  }
 
   return (
     <div className="pomodoro">
       <h1>Your are: Working</h1>
       <Timer mainTimer={mainTimer} />
       <div className="controls">
-        <Button label="Start" onClick={() => {}} className="button" />
-        <Button label="Start" onClick={() => {}} className="button" />
-        <Button label="Start" onClick={() => {}} className="button" />
+        <Button label="Work" onClick={() => configureWork()} className="button" />
+        <Button label="Rest" onClick={() => configureRest()} className="button" />
+        <Button className={!isActive ? 'hidden' : 'button'} label={isActive ? 'Stop' : 'Start'} onClick={() => setIsActive(!isActive)} />
       </div>
     </div>
   )
